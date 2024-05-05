22591291f2d6adb38f90df9ac0a01735de231fa0
Jethro <jethrosimbulan.dev@gmail.com>
2024-05-06 03:22:48 +0800

    fixing the contact form

--- contact.php
+++ contact.php
@@ -8,46 +8,46 @@ require 'PHPMailer/src/SMTP.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    //Get Input
-    $name = $_POST['Name'];
-    $attend = isset($_POST['Attend_wedding']) ? $_POST['Attend_wedding'] : '';
-    $message = $_POST['Message'];
+//Get Input
+$city = $_POST['city'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$addr = $_POST['addr'];
+$msg = $_POST['msg'];
 
-    // Create a new PHPMailer instance
-    $mail = new PHPMailer();
+// Create a new PHPMailer instance
+$mail = new PHPMailer();
 
-    // Set the mailer to use SMTP
-    $mail->isSMTP();
+// Set the mailer to use SMTP
+$mail->isSMTP();
 
-    // Configure SMTP settings
-    $mail->Host = 'smtp.titan.email'; // Replace with your SMTP server
-    $mail->SMTPAuth = true;
-    $mail->Username = 'no-reply@kleenchimney.com'; // Replace with your SMTP username
-    $mail->Password = '0$P5Q1x7DXn$'; // Replace with your SMTP password
-    $mail->SMTPSecure = 'ssl'; // If required (e.g., 'ssl' or 'tls')
-    $mail->Port = 465; // Replace with your SMTP port
+// Configure SMTP settings
+$mail->Host = 'mail.kleengreen.org'; // Replace with your SMTP server
+$mail->SMTPAuth = true;
+$mail->Username = 'no-reply@kleengreen.org'; // Replace with your SMTP username
+$mail->Password = '},Dw=Am%#TVY'; // Replace with your SMTP password
+$mail->SMTPSecure = 'ssl'; // If required (e.g., 'ssl' or 'tls')
+$mail->Port = 465; // Replace with your SMTP port
 
-    // Set the sender and recipient
-    $mail->addAddress('sd.jethro@gmail.com');
+// Set the sender and recipient
+$mail->setFrom('no-reply@kleengreen.org');
+$mail->addAddress('kleenhomeservices@gmail.com');
 
-    // Set email subject and body
-    $mail->Subject = 'Kleen Chimney RSVP';
-    $mail->Body = '
-        <p><strong>Name: </strong> ' . $name . '</p>
-        <p><strong>Attend wedding: </strong> ' . $attend . '</p>
-        <p><strong>Message: </strong> ' . $message . '</p>
-    ';
+// Set email subject and body
+$mail->Subject = 'Kleengreen ' . $city ;
+$mail->Body = '
+<p><strong>Email: </strong> ' . $email . '</p>
+<p><strong>Phone Number: </strong> ' . $phone . '</p>
+<p><strong>Address: </strong> ' . $addr . '</p>
+<p><strong>Message: </strong> ' . $msg . '</p>
+';
 
-    // Set email format to HTML
-    $mail->isHTML(true);
+// Set email format to HTML
+$mail->isHTML(true);
 
-    // Send the email
-    if ($mail->send()) {
-        echo "The email message was sent.";
-    } else {
-        echo "The email message was not sent. Error: " . $mail->ErrorInfo;
-    }
-    exit; // Prevent further execution of HTML code after form submission
+// Send the email
+if ($mail->send()) {
+    echo "The email message was sent.";
+} else {
+    echo "The email message was not sent. Error: " . $mail->ErrorInfo;
 }
-?>
